e8da60c9f019c91e495c368be7e88b741bd9aa84
Karol Hrdina <karolhrdina@eaton.com>
2016-05-30 11:20:26 +0200

    PROBLEM: 'retire' is not included in ASSET operation defines SOLUTION: add it
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- include/bios_proto.h
+++ include/bios_proto.h
@@ -122,6 +122,7 @@ The software maintains three main types of information divided to three streams
 #define BIOS_PROTO_ASSET_OP_CREATE          "create"
 #define BIOS_PROTO_ASSET_OP_UPDATE          "update"
 #define BIOS_PROTO_ASSET_OP_DELETE          "delete"
+#define BIOS_PROTO_ASSET_OP_RETIRE          "retire"
 #define BIOS_PROTO_ASSET_OP_INVENTORY       "inventory"
 #define BIOS_PROTO_RULE_CLASS               "rule_class"
 
