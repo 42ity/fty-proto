8fc6182c010b2d4ad9a04584d4ea2293f5fac79b
Mauro Guerrera <mauroguerrera@eaton.com>
2019-12-02 15:54:31 +0100

    Remove stable branch from libsodium

--- builds/check_zproject/ci_build.sh
+++ builds/check_zproject/ci_build.sh
@@ -9,7 +9,7 @@ set -ex
 # Note: certain zproject scripts that deal with deeper dependencies expect that
 # such checkouts are directly in the same parent directory as "this" project.
 cd "$REPO_DIR/.."
-git clone --quiet --depth 1 -b stable https://github.com/42ity/libsodium.git libsodium
+git clone --quiet --depth 1 https://github.com/42ity/libsodium.git libsodium
 git clone --quiet --depth 1 https://github.com/42ity/libzmq.git libzmq
 git clone --quiet --depth 1 https://github.com/42ity/czmq.git czmq
 git clone --quiet --depth 1 -b 1.0-FTY-master https://github.com/42ity/malamute.git malamute
